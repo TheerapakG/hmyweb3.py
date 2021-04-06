b715c0eb3a30b89ec04974e42729223d3e87d3ac
TheerapakG <theerapakg@gmail.com>
2021-04-06 07:22:03 +0700

    fix combing demo

--- demo/combing.py
+++ demo/combing.py
@@ -1,6 +1,8 @@
 from eth_account import Account
 from hmyweb3.bootstrap import w3
 
+e_18 = 10**18
+
 total_txs = 0
 total_one = 0
 
@@ -8,8 +10,8 @@ for privkey in range(0x000000000000000000000000000000000000000000000000000000000
     strprivkey = f'0x{privkey:0>64x}'
     account = Account.from_key(strprivkey)
     txs = w3.hmy.get_transaction_count(account.address)
-    balance = w3.hmy.get_balance(account.address)/(10^18)
+    balance = w3.hmy.get_balance(account.address)/e_18
     total_one += balance
     print('searched', privkey, 'key. found', total_one, 'ONE. and', total_txs, 'transactions.')
-    if balance > 1e18:
+    if balance > e_18:
         print('FOUND', privkey, balance, 'WITH', txs, 'TRANSACTIONS')
